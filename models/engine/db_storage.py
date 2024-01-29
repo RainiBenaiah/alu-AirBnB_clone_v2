ff52021ae9642ab4abd1801dcc3bdec6107b1ff6
irakozej <jeaniakoze68@gmail.com>
2024-01-29 21:08:39 +0000

    the db_storage.py tested

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,10 +1,10 @@
 import unittest
 import os.path
 
+
 class YourTestCase(unittest.TestCase):
 
-    @unittest.skipIf(not os.path.exists('models/engine/db_storage.py'), "File not found, skipping test")
+    @unittest.skipIf(not os.path.exists('models/engine/db_storage.py'), "exit")
     def test_something(self):
         # Your test logic here
         pass
-
