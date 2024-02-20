a90d0353c63ad75e06c6a7fb9d5ae1f5df115a23
RainiBenaiah <b.raini@alustudent.com>
2024-02-20 21:07:55 +0000

    redone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,49 +1,17 @@
 #!/usr/bin/env bash
-# Sets up the servers for deployment of the web_static
+# Install Nginx if not already installed
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 
+# create folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+#create a fake html file
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-#installing nginx if its not already installed
-if ! command -v nginx &> /dev/null
-then
-	sudo apt-get update
-	sudo apt-get install -y nginx
-fi
-
-#creating the folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-#create fake HTML file
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"|sudo tee /data/web_static/releases/test/index.html
-
-# for the symbolic link
-if [ -L "/data/web_static/current" ]; then
-	sudo rm /data/web_static/current
-fi
-
-sudo ln -s /data/web_static/releases/test/  /data/web_static/current
-
-# giving ownership to the /data/ folder
-sudo chown -R ubuntu:ubuntu /data/
-
-# updating nginx configuration
-nginx_config="server {
-    listen 80;
-    listen [::]:80;
-
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-        index index.html;
-    }
-}"
-
-echo "$nginx_config" | sudo tee /etc/nginx/sites-available/default > /dev/null
-sudo service nginx restart
+# give ownership
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
