fec0513321d41dc9d33e6c6412fcdfeafe52d170
RainiBenaiah <b.raini@alustudent.com>
2024-02-24 13:42:49 -0800

    error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,11 @@
-#!/usr/bin/python
-""" Web application listening on 0.0.0.0, port 5000 """
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
