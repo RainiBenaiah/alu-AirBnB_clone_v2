6067fa420805a53c3ebbd690c9776b1fa8cc7bb0
irakozej <jeaniakoze68@gmail.com>
2024-02-24 13:41:32 +0000

    .

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
